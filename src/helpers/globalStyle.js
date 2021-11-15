c68925f2bc5716c7cd1008f2fb6175673cbaa9b7
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-11-15 21:35:25 +0100

    Remove unusued global css from globalStyle

--- src/helpers/globalStyle.js
+++ src/helpers/globalStyle.js
@@ -4,24 +4,8 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.colors.body};
     color: ${({ theme }) => theme.colors.text};
-    font-family: ${({ theme }) => theme.font};
+    /* font-family: ${({ theme }) => theme.font}; */
     transition: all 0.35s linear;
   }
-
-  button {
-    border: 0;
-    display: inline-block;
-    padding: 12px 24px;
-    font-size: 14px;
-    border-radius: 4px;
-    margin-top: 5px;
-    cursor: pointer;
-    background-color: #1064EA;
-    color: #FFFFFF;
-  }
-
-  button.btn {
-    background-color: ${({ theme }) => theme.colors.button.background};
-    color: ${({ theme }) => theme.colors.button.text};
-  }
+  
 `;
